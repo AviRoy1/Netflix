a8dd8ff33592102e79c9425a2e781e828eeb258d
AviRoy1 <avijitroy2944@gmail.com>
2026-05-12 21:36:53 +0530

    video event consume service for encoding.

--- encoding-service/src/main/java/com/netflix/encodingservice/service/VideoEventConsumer.java
+++ encoding-service/src/main/java/com/netflix/encodingservice/service/VideoEventConsumer.java
@@ -1,4 +1,30 @@
 package com.netflix.encodingservice.service;
 
+import com.netflix.encodingservice.event.VideoUploadedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class VideoEventConsumer {
+
+    private final EncodingService encodingService;
+
+    @KafkaListener(
+            topics = "video.uploaded",
+            groupId = "encoding-service-group"
+    )
+    public void consumeVideoUploadedEvent(VideoUploadedEvent event) {
+
+        log.info("Consumed VideoUploaded event for movie: {} file: {}" , event.getMovieId(), event.getOriginalFileName());
+        try {
+            encodingService.encodeVideo(event);
+        } catch(Exception e) {
+            log.error("Failed to process encoding the movie: {} - {}" , event.getMovieId(), e.getMessage());
+        }
+
+    }
 }
